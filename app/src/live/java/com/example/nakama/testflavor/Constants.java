e76d5104d6e4162122c4f116fae0a87369cc241f
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2017-12-08 13:13:09 +0700

    Initial commit

---
+++ app/src/live/java/com/example/nakama/testflavor/Constants.java
@@ -0,0 +1,5 @@
+package com.example.nakama.testflavor;
+
+public class Constants {
+    public static final String a = "a";
+}
---
+++ mylibrary/src/staging/java/com/example/mylibrary/Test.java
@@ -0,0 +1,9 @@
+package com.example.mylibrary;
+
+/**
+ * Created by nakama on 08/12/17.
+ */
+
+public class Test {
+    public static final String a = "bbb";
+}
